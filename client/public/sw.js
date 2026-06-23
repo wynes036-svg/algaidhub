cf9e08c0cd11bc5686c1e15b8a617c3b24beb729
wayne <wynes036@gmail.com>
2026-06-23 19:11:36 +0300

    fix service worker breaking movie requests

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,21 +1,4 @@
-const CACHE = "algaidhub-v1";
-const PRECACHE = ["/", "/index.html"];
-
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(PRECACHE)));
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", (e) => {
-  e.waitUntil(caches.keys().then((keys) =>
-    Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (e) => {
-  if (e.request.method !== "GET") return;
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
-});
+// Minimal service worker - just enables PWA install, no caching interference
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", () => self.clients.claim());
+// No fetch handler - let all requests pass through normally
